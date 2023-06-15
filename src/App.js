91aacd08830a358f61f7f5dd3fda57084ea30acb
MariiaRo <126449306+MariiaRo@users.noreply.github.com>
2023-06-15 18:04:03 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import SortingVisualizer from './sortingvisualizer/SortingVisualizer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualizer></SortingVisualizer>
+    </div>
+  );
+}
+
+export default App;
